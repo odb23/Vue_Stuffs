4d66e9000f138d0ffc571af92748a159c6e83973
Odulaja oluwadamilare O <damijustice1@gmail.com>
2022-09-29 17:25:14 +0100

    implemented Tasks CRUD with fake server

--- task-manager/src/App.vue
+++ task-manager/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="container">
-    <HeaderComponent name="Task Tracker" @toggle-add-task="toggleAddTask" :showAddTask="showAddTask"/>
+    <HeaderComponent name="Task Tracker" @toggle-add-task="toggleAddTask" :showAddTask="showAddTask" />
 
     <div v-if="showAddTask">
       <AddTask @add-task="addTask" />
@@ -30,35 +30,69 @@ export default {
   },
   methods: {
 
-    deleteTask(id) {
+    async deleteTask(id) {
       if (confirm('Are you sure')) {
-        this.tasks = this.tasks.filter(task => task.id !== id);
+        const res = await fetch('http://localhost:5031/tasks/' + id, {
+          method: 'DELETE',
+        });
+
+        res.status === 200 && (this.tasks = this.tasks.filter(task => task.id !== id));
       }
     },
 
-    toggleReminder(id) {
-      console.log("Just got here")
-      this.tasks = this.tasks.map(t => t.id === id ? { ...t, reminder: !t.reminder } : t)
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id)
+      const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+      const res = await fetch('http://localhost:5031/tasks/' + id, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(updTask),
+      })
+      const data = await res.json()
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
     },
 
-    addTask(newTask) {
-      this.tasks = [...this.tasks, newTask]
-      this.toggleAddTask()
+    async addTask(newTask) {
+      console.log(newTask)
+      const res = await fetch('http://localhost:5031/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(newTask)
+      });
+
+      const data = await res.json()
+
+      if (data) {
+        this.tasks = [...this.tasks, data]
+        this.toggleAddTask()
+      }
     },
 
     toggleAddTask() {
       this.showAddTask = !this.showAddTask
+    },
+
+    async fetchTasks() {
+      const res = await fetch('http://localhost:5031/tasks');
+      const data = await res.json();
+
+      return data
+    },
+
+    async fetchTask(id) {
+      const res = await fetch('http://localhost:5031/tasks/' + id);
+      const data = await res.json();
+      return data
     }
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: "Doctor's Appointment",
-        day: 'March 1st at 2:30pm',
-        reminder: true,
-      }
-    ]
+  async created() {
+    this.tasks = await this.fetchTasks()
   },
 
 }
